76bdc4cfcd0c926c5bdfcf10906641f0e4f8fbf5
Leandro Oliveira <leandro.gooli@gmail.com>
2025-02-07 21:56:14 +0000

    Aula 01

---
+++ Aula01/main.js
@@ -0,0 +1,22 @@
+const profile = {
+  name: "", 
+  age: 0, 
+  transations: [], 
+  balance: 0, 
+}
+
+const project ={
+  id: 0,
+  name: "",
+  description: "",
+  tag: "", 
+  data: new Date().toLocaleDateString(), 
+}
+
+const  transaction = {
+  id: 0,
+  product: product,
+  user: user,
+  quantity: 0,
+  total: 0,
+}
